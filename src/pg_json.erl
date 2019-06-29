2e98a0de2e9417b34d8451fe9ab68e91461c876d
Tristan Sloughter <t@crashfast.com>
2019-06-29 07:29:56 -0600

    support configuring a json lib to encode/decode

--- src/pg_json.erl
+++ src/pg_json.erl
@@ -6,13 +6,22 @@
          encode/2,
          decode/2]).
 
+-include("pg_types.hrl").
 -include("pg_protocol.hrl").
 
+init(#{json_config := Config}) ->
+    {[<<"json_send">>], Config};
 init(_Opts) ->
-    {[<<"json_send">>], []}.
+    Config = application:get_env(pg_types, json_config, []),
+    {[<<"json_send">>], Config}.
 
-encode(Json, _) ->
-    [<<(iolist_size(Json)):?int32>>, Json].
+encode(Json, #type_info{config=[]}) ->
+    [<<(iolist_size(Json)):?int32>>, Json];
+encode(Json, #type_info{config={Module, EncodeOptions, _}}) ->
+    EncodedJson = Module:encode(Json, EncodeOptions),
+    [<<(iolist_size(EncodedJson)):?int32>>, EncodedJson].
 
-decode(Bin, _) ->
-    Bin.
+decode(Bin, #type_info{config=[]}) ->
+    Bin;
+decode(Bin, #type_info{config={Module, _, DecodeOptions}}) ->
+    Module:decode(Bin, DecodeOptions).
--- src/pg_jsonb.erl
+++ src/pg_jsonb.erl
@@ -6,15 +6,24 @@
          encode/2,
          decode/2]).
 
+-include("pg_types.hrl").
 -include("pg_protocol.hrl").
 
 -define(JSONB_VERSION_1, 1).
 
+init(#{json_config := Config}) ->
+    {[<<"json_send">>], Config};
 init(_Opts) ->
-    {[<<"jsonb_send">>], []}.
+    Config = application:get_env(pg_types, json_config, []),
+    {[<<"jsonb_send">>], Config}.
 
-encode(Json, _) ->
-    [<<(iolist_size(Json) + 1):?int32, ?JSONB_VERSION_1:?int8>> | Json].
+encode(Json, #type_info{config=[]}) ->
+    [<<(iolist_size(Json) + 1):?int32, ?JSONB_VERSION_1:?int8>> | Json];
+encode(Json, #type_info{config={Module, EncodeOptions, _}}) ->
+    EncodedJson = Module:encode(Json, EncodeOptions),
+    [<<(iolist_size(EncodedJson) + 1):?int32, ?JSONB_VERSION_1:?int8>> | EncodedJson].
 
-decode(<<?JSONB_VERSION_1:?int8, Bin/binary>>, _) ->
-    Bin.
+decode(<<?JSONB_VERSION_1:?int8, Bin/binary>>, #type_info{config=[]}) ->
+    Bin;
+decode(<<?JSONB_VERSION_1:?int8, Bin/binary>>, #type_info{config={Module, _, DecodeOptions}}) ->
+    Module:decode(Bin, DecodeOptions).
